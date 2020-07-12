672ddc97b7702ea5445de2695b72fa7a684a1366
fcsouza <fcsouza18@hotmail.com>
2020-07-12 20:55:55 -0300

    graphQL-api

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressGraphQL = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const serverless = require("serverless-http");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "HelloWorld",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "Hello World",
+      },
+    }),
+  }),
+});
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  "/",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
 module.exports.handler = serverless(app);
