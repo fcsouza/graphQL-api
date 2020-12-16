895656f65486fba1203aaf4f5c5b9a472424ecdb
fcsouza <fcsouza18@hotmail.com>
2020-12-16 19:57:36 -0300

    include server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import './utils/connection';
+import { buildSchema } from 'type-graphql'
+import { ApolloServer } from 'apollo-server'
+
+
+async function bootstrap(){
+  const schema = await buildSchema({
+    resolvers: []
+  })
+
+  const server = new ApolloServer({schema});
+
+  server.listen({port: 4100}, () => console.log("Running"));
+}
+
+
+bootstrap();
